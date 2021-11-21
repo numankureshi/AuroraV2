b53fcefb0c6dcdd201bfabffe76ef84d113dbc0b
Gaurav Golatkar <ggolatkar@ZISYSTECH.COM>
2021-11-21 22:42:13 +0530

    Maintaince done in research smoke, added missing directories in suite.

---
+++ Engage/Research-Smoke/Research-Smoke.bat
@@ -0,0 +1,4 @@
+title Research-Smoke Execution
+cd E:\My Projects\engage\Engage\Research-Smoke
+CALL E:\Automation\automation_setup\apache-maven-3.6.3-bin\apache-maven-3.6.3\bin\mvn.cmd clean test
+pause
