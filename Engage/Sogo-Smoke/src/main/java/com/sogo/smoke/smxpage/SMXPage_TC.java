2b4dea11a36de3509caceeaa5e34596b01b58dd8
Gaurav Golatkar <ggolatkar@zarca.com>
2022-06-08 13:46:13 +0530

    Test commit - Vivek

--- Engage/Sogo-Smoke/src/main/java/com/sogo/smoke/smxpage/SMXPage_TC.java
+++ Engage/Sogo-Smoke/src/main/java/com/sogo/smoke/smxpage/SMXPage_TC.java
@@ -48,7 +48,7 @@ public class SMXPage_TC extends SuiteBase {
 		}
 	}
 
-
+		//Vivek Comment
 		@Test(dataProvider = "SurveyPage", dataProviderClass = utility.XLSDataProvider.class, groups = "smxpage", alwaysRun = true)
 		public void Smoke_TC2(LinkedHashMap<String, String> data) throws Exception {
 			TestCaseName = getData(data, "TestCaseName");
