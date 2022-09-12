128cf16656e64f76d99d83e8b993f3124e2dda1e
Vivek Grandhi <vgrandhi@sogolytics.com>
2022-09-12 16:02:07 +0530

    Multi Question Branching data validation

--- Engage/Engage-Smoke/src/main/java/com/engage/smoke/smxpage/SMXPage_TC.java
+++ Engage/Engage-Smoke/src/main/java/com/engage/smoke/smxpage/SMXPage_TC.java
@@ -3439,6 +3439,44 @@ public class SMXPage_TC extends SuiteBase {
 
 	}
 	
+	
+	@Test(dataProvider = "SurveyPage", dataProviderClass = utility.XLSDataProvider.class, groups = "smxpage", alwaysRun = true)
+	public void Smoke_TC109(LinkedHashMap<String, String> data) throws Exception {
+		TestCaseName = getData(data, "TestCaseName");
+		test = extent.createTest(TestCaseName);
+		CaseToRun = getData(data, "CaseToRun");
+		String Role = getData(data, "Role");
+		//test
+		
+		HashMap<String, String> param = new HashMap<String, String>();
+		param.put("surveyname", getData(data, "surveyname"));
+		param.put("AnswerOptions", getData(data, "AnswerOptions"));
+		param.put("TextBox", getData(data, "TextBox"));
+		if (CaseToRun.equalsIgnoreCase("N")) {
+			System.out.println("CaseToRun = N for " + TestCaseName + "So skipping Exceution.");
+			testSkip = true;
+			test.skip("CaseToRun = N for " + TestCaseName + "So skipping Exceution.");
+			throw new SkipException("CaseToRun = N for " + TestCaseName + "So skipping Exceution.");
+		} else {
+			for (String key : URLs.keySet()) {
+				System.out.println(URLs.get(key));
+				credentials = TestFile.getLoginCredentials("Users", Role);
+				for (int i = 0; i < credentials.size(); i++) {
+					users = credentials.get(i);
+					username = users.get("username");
+					encPassword = users.get("password");
+				}
+//				password = decryptPass.decryptUserPassword(encPassword);
+				
+				loadBrowser();
+				loginPage.login(getDriver(), param, username, encPassword, URLs.get(key), test);
+				smxPage.mQB1(getDriver(), param, test);
+				
+			}
+		}
+
+	}
+	
 	@AfterMethod(alwaysRun = true)
 	public void reporterDataResults(ITestResult Result) throws IOException {
 		if (Result.getStatus() == ITestResult.SKIP) {
